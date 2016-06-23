186499ac7fc90077535ba48224d2b39d45138fbf
Du, Changbin <changbin.du@gmail.com>
2016-06-23 20:14:43 +0800

    improve setup.sh
    Signed-off-by: Du, Changbin <changbin.du@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
-echo "Installation will take some time, please wait!"
 
-echo -n "Will remove old ~/.vim files[PRESS ENTER TO CONTINUE]:"
-read confirm
-if [ -n "$confirm" ]; then
-	echo "terminated"
-	exit 1
+update=false
+
+if [ -f ~/.vim/tips.md ] && [ -d ~/.vim/bundle/Vundle.vim ]; then
+    echo "Seems you have already installed me, update or reinstall?"
+    select ur in "update" "reinstall"; do
+        case $ur in
+            update ) update=true; break;;
+            reinstall ) break;;
+        esac
+    done
 fi
 
-rm ~/.vim -rf && mkdir -p ~/.vim
+if [ $update == false ]; then
+    echo -e "\033[31mDo you wish to remove old ~/.vim files?\033[0m"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes )
+                 echo "remove ~/.vim folder..." 
+                 rm ~/.vim -rf && mkdir -p ~/.vim
+                 break;;
+            No )
+                 echo "terminated"
+                 exit;;
+        esac
+    done
+fi
+
+echo "Installation will take some time, please wait!"
 cp .vimrc ~/.vimrc
 cp tips.md ~/.vim/
 
 sudo apt-get install python-pip vim vim-nox exuberant-ctags git astyle
 sudo pip install dbgp vim-debug pep8 flake8 pyflakes isort
 
-echo "Installing Vundle..."
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ $update == false ]; then
+    echo "Installing Vundle..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "Installing plugins"
-vim +PluginInstall +qall
+    echo "Installing plugins"
+    vim +PluginInstall +qall
+else
+    echo "Update plugins"
+    vim +PluginUpdate
+fi
+
 echo "All done, enjoy with pow-vim!"
 
